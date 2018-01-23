fea9366052732c91dcadb1d799aee7ed514beac2
kandhan <kandhan.kuhan@gmail.com>
2018-01-23 17:31:17 +0530

    speed test added with report #2

--- jobs/jobs_to_do/test_speed.py
+++ jobs/jobs_to_do/test_speed.py
@@ -39,46 +39,59 @@ hundred_t = (timeit.timeit(dot_product_100000_to_100000, number=2, setup="import
 
 
 report = """
-Performance Test:
+<html>
+<body>
+<div>
+<h2>Performance Test:</h2>
 
--------1--------
-Number of layers: 2
-No. of neurons in layer 1: 1000
-No. of neurons in layer 2: 1000
-Connections: Fully connected ([1000,1000] connection matrix)
-Operation: Calculate total input to Layer 2 neurons from Layer 1 by 
-multiplying Layer 1 neurons with appropriate connections (dot product)
-Time taken in seconds(best from three tries): {}
+<h3>-------1--------</h3>
 
+<strong>Number of layers:</strong> 2 <br/>
+<strong>No. of neurons in layer 1:</strong> 1000 <br/>
+<strong>No. of neurons in layer 2:</strong> 1000 <br/>
+<strong>Connections:</strong> Fully connected ([1000,1000] connection matrix) <br/>
+<strong>Operation:</strong> Calculate total input to Layer 2 neurons from Layer 1 by 
+multiplying Layer 1 neurons with appropriate connections (dot product) <br/>
+<strong>Time taken in seconds(best from three tries):</strong> {} <br/>
+<br/><br/>
 
-----------2------------
-Number of layers: 2
-No. of neurons in layer 1: 5000
-No. of neurons in layer 2: 5000
-Connections: Fully connected ([5000,5000] connection matrix)
-Operation: Calculate total input to Layer 2 neurons from Layer 1 by 
-multiplying Layer 1 neurons with appropriate connections (dot product)
-Time taken in seconds(best from three tries): {}
+<h3>----------2------------</h3>
 
-
-----------------------3----------------
-Number of layers: 2
-No. of neurons in layer 1: 10000
-No. of neurons in layer 2: 10000
-Connections: Fully connected ([10000,10000] connection matrix)
-Operation: Calculate total input to Layer 2 neurons from Layer 1 by 
-multiplying Layer 1 neurons with appropriate connections (dot product)
-Time taken in seconds(best from three tries): {}
+<strong>Number of layers:</strong> 2 <br/>
+<strong>No. of neurons in layer 1:</strong> 5000 <br/>
+<strong>No. of neurons in layer 2:</strong> 5000 <br/>
+<strong>Connections:</strong> Fully connected ([5000,5000] connection matrix) <br/>
+<strong>Operation:</strong> Calculate total input to Layer 2 neurons from Layer 1 by 
+multiplying Layer 1 neurons with appropriate connections (dot product) <br/>
+<strong>Time taken in seconds(best from three tries):</strong> {} <br/>
+<br/><br/>
 
 
-----------------4-----------------------
-Number of layers: 2
-No. of neurons in layer 1: 100000
-No. of neurons in layer 2: 100000
-Connections: Fully connected ([100000,100000] connection matrix)
-Operation: Calculate total input to Layer 2 neurons from Layer 1 by 
-multiplying Layer 1 neurons with appropriate connections (dot product)
-Time taken in seconds(best from three tries): {} 
+<h3>----------------------3------------</h3>
+
+<strong>Number of layers:</strong> 2 <br/>
+<strong>No. of neurons in layer 1:</strong> 1000 <br/>
+<strong>No. of neurons in layer 2:</strong> 1000 <br/>
+<strong>Connections:</strong> Fully connected ([1000,1000] connection matrix) <br/>
+<strong>Operation:</strong> Calculate total input to Layer 2 neurons from Layer 1 by 
+multiplying Layer 1 neurons with appropriate connections (dot product) <br/>
+<strong>Time taken in seconds(best from three tries):</strong> {} <br/>
+<br/><br/>
+
+<h3>----------------4-----------------------</h3>
+
+<strong>Number of layers:</strong> 2 <br/>
+<strong>No. of neurons in layer 1:</strong> 100000 <br/>
+<strong>No. of neurons in layer 2:</strong> 100000 <br/>
+<strong>Connections:</strong> Fully connected (<strong>[100000,100000] connection matrix</strong>) <br/>
+<strong>Operation:</strong> Calculate total input to Layer 2 neurons from Layer 1 by 
+multiplying Layer 1 neurons with appropriate connections (dot product) <br/>
+<strong>Time taken in seconds(best from three tries):</strong> {} <br/>
+<br/><br/>
+
+</div>
+</body>
+</html>
 """.format(thousand, five_t, ten_t, hundred_t)
 
 
@@ -90,4 +103,4 @@ send_mail('reports@mail.cyces.co', (
         'thecorptechteam@gmail.com',
         'vidushi.meenu@gmail.com',
         'rootat1306@gmail.com',
-    ), subject='Performance test on Monster Machine', message=report)
+    ), subject='Performance test on Monster Machine #2', message=report)
